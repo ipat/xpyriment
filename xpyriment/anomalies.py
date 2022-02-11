87fc0e17cfdc8e8c6c014f091719bb5fd365f003
Pat Hornchaiya <phornchaiya@Pats-MacBook-Air.local>
2022-02-11 21:56:24 +0700

    Small update on SRM function to return results

--- xpyriment/anomalies.py
+++ xpyriment/anomalies.py
@@ -7,19 +7,26 @@ import numpy as np
 test = {"a": "b", "c": "d", "ab": "s", "age": 28}
 
 
+# According to https://www.microsoft.com/en-us/research/group/experimentation-platform-exp/articles/diagnosing-sample-ratio-mismatch-in-a-b-testing/
+# they use 0.0005 as the critical value to detect if SRM exists. Therefore, we set the default critical_value to resemble to the study.
 def detect_srm(
     df: pd.DataFrame,
     variant_col: str = "variant",
     result_col: str = "result",
     value_cols: List[str] = [],
     detect_cols: List[str] = [],
-    critical_value: float = 0.05,
+    critical_value: float = 0.0005,
 ):
+
+    # the calculation approach is referred to Twitter engineering page:
+    # https://blog.twitter.com/engineering/en_us/a/2015/detecting-and-avoiding-bucket-imbalance-in-ab-tests
 
     if detect_cols == []:
         detect_cols = df.columns.tolist().remove(value_cols)
 
-    print(detect_cols)
+    results_df = pd.DataFrame(
+        columns=["detecting_col", "chi2_stat", "p_value", "is_srm"]
+    )
 
     # for each col calculate SRM statistics
     for each_feature in detect_cols:
@@ -31,15 +38,21 @@ def detect_srm(
             values=result_col,
         )
 
-        stat, p, dof, expected_arr = chi2_contingency(this_df)
+        chi2_stat, p, dof, expected_arr = chi2_contingency(this_df)
 
         if p <= critical_value:
-            # TODO: Implement 2-side binomial test
-            pass
+            found_srm = True
+        else:
+            found_srm = False
 
-        # the calculation approach is referred to Twitter engineering page:
-        # https://blog.twitter.com/engineering/en_us/a/2015/detecting-and-avoiding-bucket-imbalance-in-ab-tests
+        results_df = results_df.append(
+            {
+                "detecting_col": each_feature,
+                "chi2_stat": chi2_stat,
+                "p_value": p,
+                "is_srm": found_srm,
+            },
+            ignore_index=True,
+        )
 
-        pass
-
-    pass
+    return results_df
